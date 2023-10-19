b131ca1d7d578dd3c34459181a904fb8937604f9
master <yarasmoothbeat@gmail.com>
2023-10-19 12:32:11 +0500

    66

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,12 @@
+﻿int NumbersAmount(int M, int N)
+{
+    int sum = 0;
+    while (M <= N)
+    {
+        sum += M;
+        M++;
+    }
+    return sum;
+}
+
+Console.WriteLine(NumbersAmount(4, 8));
