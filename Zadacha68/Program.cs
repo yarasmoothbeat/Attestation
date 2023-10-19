fdb7adeb618090ee5f6b88c7e3516a54156abcfe
master <yarasmoothbeat@gmail.com>
2023-10-19 13:53:52 +0500

    68

---
+++ Zadacha68/Program.cs
@@ -0,0 +1,17 @@
+﻿int AkkermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+       return n + 1;
+    }
+    else if (n == 0)
+        {
+            return AkkermanFunction(m - 1, 1);
+        }
+        else
+        {
+            return AkkermanFunction(m - 1, AkkermanFunction(m, n -1));
+        }
+}
+
+Console.WriteLine(AkkermanFunction(3, 2));
